dcbba8ee1d2f7ea53aaafa1fbb7af011a8e2f52d
Philip Hale <p.hale.09@aberdeen.ac.uk>
2011-02-07 17:09:42 +0000

    end of practical.  exercise pretty much complete. todo: prevent more than 120 people from boarding the plane

--- CheckInDesk.java
+++ CheckInDesk.java
@@ -9,31 +9,61 @@ public class CheckInDesk  {
     UserDialog userDialog;
     private int passengerCount;
     private int baggageCount;
-    public static final int MAX_BAGGAGE = 3000;
+    private int excessBaggageLeft;
+    private int maxExcessBaggage;
+
+    public static final int MAX_BAGGAGE = 3000;    
     public static final int MAX_PASSENGERS = 120;
+    public static final int FREE_BAGGAGE = 20;
+    public static final int EXCESS_BAGGAGE_PRICE = 1;
+   
 
     public CheckInDesk()  {
         tickets = new ArrayList();
         userDialog = new UserDialog();
+        maxExcessBaggage = MAX_BAGGAGE - FREE_BAGGAGE*MAX_PASSENGERS;
+        excessBaggageLeft = maxExcessBaggage;
 
     }
-    
+
     public void newTicket()  {
         String[] options ={"meal", "drink", "budget"};
-        char ticket = userDialog.selectString("Please select a ticket type from the options below:", options).charAt(0);
-        int baggage = userDialog.getInt("How much baggage does the passenger have?"); 
+        char ticket = 'a';
+        String ticketString = "4";
+        while (!ticketString.equals(""))  {
+             ticketString = userDialog.selectString("Please select a ticket type from the options below:", options);
+             ticket = ticketString.charAt(0);
+            }
+        int baggage = userDialog.getInt("How much baggage does the passenger have?");
+        int baggageCost = 0;
+        if (baggage>20)  {
+            if (baggage <= (excessBaggageLeft))  {
+                baggageCost = (baggage - 20) * EXCESS_BAGGAGE_PRICE;
+                userDialog.showMessage("There will be an excess baggage charge of: £" + baggageCost);
+                excessBaggageLeft -= baggage;
+            }
+            else  {
+                userDialog.showMessage("Sorry, but there is only: " + excessBaggageLeft + "kg of free baggage left.");
+                return;
+            }
+        }
+        
+        
+        
         switch (ticket)  {
-            case 'm': tickets.add(new Ticket('m', baggage)); break;
-            case 'd': tickets.add(new Ticket('d', baggage)); break;
-            case 'b': tickets.add(new Ticket('b', baggage)); break;
-            default: System.out.println("Something's wrong....");
+            case 'm': tickets.add(new Ticket('m', baggage, 60 + baggageCost)); break;
+            case 'd': tickets.add(new Ticket('d', baggage, 55 + baggageCost)); break;
+            case 'b': tickets.add(new Ticket('b', baggage, 50 + baggageCost)); break;
+            default: return; 
         }
         passengerCount++;
         baggageCount+= baggage;
     }
-    
-  //  private int returnPrice(Ticket ticket)  {
-        
-    }
-    
+
+}
+
+//  private int returnPrice(Ticket ticket)  {
+
+
+
     
--- Ticket.java
+++ Ticket.java
@@ -5,11 +5,13 @@ public class Ticket  {
     private int baggage;
     // There are 3 ticket types.  m: meal d: drink b: budget
     private char ticketType;
+    private int ticketPrice;
     
-    public Ticket(char ticketType, int baggage)  {
+    public Ticket(char ticketType, int baggage, int ticketPrice)  {
         this.ticketType = ticketType;
         this.baggage = baggage;
-        
+        System.out.println(toString());
+        this.ticketPrice = ticketPrice;
     }
     
     public char getTicketType()  {
@@ -19,6 +21,19 @@ public class Ticket  {
     public int getBaggage()  {
         return baggage;
     }
+    
+    public String toString()  {
+        String tT = "";
+        switch (ticketType)  {
+            case 'm': tT = "In-flight meal"; break; 
+            case 'd': tT = "In-flight drink"; break;
+            case 'b': tT = "Budget"; break;
+        }
+        
+        String s = "----------\nTicket type: " + tT + "\nBaggage: " + baggage + "kg\n" + "£" + ticketPrice + "\n----------";
+        return s;
+    }
+    
 }
     
     
