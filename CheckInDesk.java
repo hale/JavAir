c51c33d54bbdc558453eb0b34aa88fb10c0dd114
Philip Hale <p.hale.09@aberdeen.ac.uk>
2011-02-08 14:55:01 +0000

    issuing tickets now continuous, system recognises when flight is full

--- CheckInDesk.java
+++ CheckInDesk.java
@@ -3,15 +3,21 @@ import java.util.ArrayList;
 /**
  * The CheckInDesk class is responsible for issuing tickets and keeping track 
  * of the number of tickets sold, the amount of baggage on the plane.  
+ * 
+ * @author Philip Hale
+ * @version p1
  */
 public class CheckInDesk  {
     private ArrayList<Ticket> tickets;
     UserDialog userDialog;
     private int passengerCount;
     private int baggageCount;
+    // In order to ensure that the each passenger gets their allocated 20kg of baggage,
+    // excess baggage capacity is differentiated from total baggage capacity.
     private int excessBaggageLeft;
     private int maxExcessBaggage;
-
+    
+    private static final String[] OPTIONS ={"meal", "drink", "budget"};
     public static final int MAX_BAGGAGE = 3000;    
     public static final int MAX_PASSENGERS = 120;
     public static final int FREE_BAGGAGE = 20;
@@ -20,19 +26,15 @@ public class CheckInDesk  {
     public CheckInDesk()  {
         tickets = new ArrayList();
         userDialog = new UserDialog();
-        maxExcessBaggage = MAX_BAGGAGE - FREE_BAGGAGE*MAX_PASSENGERS;
+        maxExcessBaggage = (MAX_BAGGAGE - FREE_BAGGAGE) * MAX_PASSENGERS;
         excessBaggageLeft = maxExcessBaggage;
-
     }
 
+    /**
+     * Create a new ticket. 
+     */
     public void newTicket()  {
-        String[] options ={"meal", "drink", "budget"};
-        char ticket = 'a';
-        String ticketString = "4";
-
-        ticketString = userDialog.selectString("Please select a ticket type from the options below:", options);
-        ticket = ticketString.charAt(0);
-
+        char ticket = userDialog.selectString("Please select a ticket type from the options below:", OPTIONS).charAt(0);
         int baggage = userDialog.getInt("How much baggage does the passenger have?");
         int baggageCost = 0;
         if (baggage>20)  {
@@ -46,19 +48,21 @@ public class CheckInDesk  {
                 return;
             }
         }
-
         switch (ticket)  {
             case 'm': tickets.add(new Ticket('m', baggage, 60 + baggageCost)); break;
             case 'd': tickets.add(new Ticket('d', baggage, 55 + baggageCost)); break;
             case 'b': tickets.add(new Ticket('b', baggage, 50 + baggageCost)); break;
             default: return; 
         }
-        passengerCount++;
         baggageCount+= baggage;
+        passengerCount++;
+        if (passengerCount == MAX_PASSENGERS)  {
+            System.out.println("Flight closed.  The following tickets have been sold for the commencing flight:");
+            for (Ticket t : tickets)  {
+                System.out.println(t);
+            }
+        }
+        else newTicket();
     }
 
-}
-
-//  private int returnPrice(Ticket ticket)  {
-
-    
+}    
