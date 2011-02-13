8a2a0c9587ca2c8c5336bc0a2c4a7175812be129
Philip Hale <p.hale.09@aberdeen.ac.uk>
2011-02-13 14:32:44 +0000

    added documentation and some minor tweaks and cleanups

--- Ticket.java
+++ Ticket.java
@@ -2,27 +2,21 @@
  * The Ticket class represents an airplane ticket.  
  * 
  * @author Philip Hale
- * @version p1
+ * @version FINAL
  */
 public class Ticket  {
-    private int baggage;
     // There are 3 ticket types.  0: meal 1: drink 2: budget
     private int ticketType;
     private int ticketPrice;
 
-    public Ticket(int ticketType, int baggage, int ticketPrice)  {
+    public Ticket(int ticketType, int ticketPrice)  {
         this.ticketType = ticketType;
-        this.baggage = baggage;
         this.ticketPrice = ticketPrice;
         System.out.println(toString());
     }
 
     public int getTicketType()  {
         return ticketType;
-    }
-
-    public int getBaggage()  {
-        return baggage;
     }
 
     public String toString()  {
@@ -33,7 +27,7 @@ public class Ticket  {
             case 2: tT = "Budget"; break;
         }
 
-        String s = "--------------------\n  *" + tT + "*\n   " + baggage + "kg baggage\n" + "   £" + ticketPrice + "\n--------------------";
+        String s = "--------------------\n  *" + tT + "*\n  £" + ticketPrice + "\n--------------------";
         return s;
     }
 
