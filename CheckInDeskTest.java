8712ccc90fe0b7ce4532fd0b949cfd51260d2579
Philip Hale <p.hale.09@aberdeen.ac.uk>
2011-02-13 13:32:18 +0000

    baggage refactoring completed

---
+++ CheckInDeskTest.java
@@ -0,0 +1,35 @@
+
+
+/**
+ * The test class CheckInDeskTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class CheckInDeskTest extends junit.framework.TestCase
+{
+    /**
+     * Default constructor for test class CheckInDeskTest
+     */
+    public CheckInDeskTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    protected void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    protected void tearDown()
+    {
+    }
+}
